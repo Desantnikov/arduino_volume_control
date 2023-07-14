56acbcfbe62416658125bd591ef4225f06a9441e
Eboklak <desiatnikovwork@gmail.com>
2023-07-14 05:30:16 +0300

    cleaned a bit

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import time
-from threading import Thread
 
 from pymata4 import pymata4
 
 
-trigger_pin = 2
-echo_pin = 4
-led_pin = 5
+TRIGGER_PIN = 2
+ECHO_PIN = 4
+LED_PIN = 5
 
 board = pymata4.Pymata4()
 
 
-def the_callback(data):
-
+def sonar_callback(data):
     print(f'Distance: {data[2]}cm')
     # tim
 
 
-# board.digital_write(led_pin, 1)
-board.set_pin_mode_sonar(trigger_pin, echo_pin, the_callback)
-board.set_pin_mode_digital_output(led_pin)
+board.set_pin_mode_sonar(TRIGGER_PIN, ECHO_PIN, sonar_callback)
+
+board.set_pin_mode_digital_output(LED_PIN)
 
 while True:
-    print('lOOP MAIN START')
-    try:
-        board.digital_write(led_pin, 1)
-        time.sleep(3)
-        board.sonar_read(trigger_pin)
+    # print(f'Main loop start')
 
-        board.digital_write(led_pin, 0)
-    except:
-        print(f'All broken')
-        board.shutdown()
+    board.digital_write(LED_PIN, 1)
+    time.sleep(3)
+    board.sonar_read(TRIGGER_PIN)
+
+    board.digital_write(LED_PIN, 0)
 
     time.sleep(5)
