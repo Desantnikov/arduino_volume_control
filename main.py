4b7c924666f57ba5a40eb7e46a5ccdcd0109baef
Eboklak <desiatnikovwork@gmail.com>
2023-07-14 05:24:08 +0300

    example working led without threading

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import threading
 import time
 from threading import Thread
 
@@ -11,39 +10,27 @@ led_pin = 5
 
 board = pymata4.Pymata4()
 
-board.set_pin_mode_digital_output(led_pin)
 
 def the_callback(data):
-    # board.digital_write(led_pin, 0)
+
     print(f'Distance: {data[2]}cm')
     # tim
 
 
-def main_thread():
-    while True:
-        print('lOOP MAIN START')
-        try:
-            board.sonar_read(trigger_pin)
-        except:
-            print(f'All broken')
-            board.shutdown()
-
-        time.sleep(5)
-
-
-def second_thread():
-    print('Second thread')
-    board.digital_write(led_pin, 1)
-    time.sleep(5)
-    board.digital_write(led_pin, 0)
-
-
 # board.digital_write(led_pin, 1)
 board.set_pin_mode_sonar(trigger_pin, echo_pin, the_callback)
+board.set_pin_mode_digital_output(led_pin)
 
+while True:
+    print('lOOP MAIN START')
+    try:
+        board.digital_write(led_pin, 1)
+        time.sleep(3)
+        board.sonar_read(trigger_pin)
 
-main_thread = threading.Thread(target=main_thread)
-secondary_thread = threading.Thread(target=second_thread)
-secondary_thread.start()
+        board.digital_write(led_pin, 0)
+    except:
+        print(f'All broken')
+        board.shutdown()
 
-main_thread.start()
+    time.sleep(5)
